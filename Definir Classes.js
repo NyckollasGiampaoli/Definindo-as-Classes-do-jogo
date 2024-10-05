e1f225d183475152c635a3db172a7286bc930d89
Nyckollas Giampaoli <122179728+NyckollasGiampaoli@users.noreply.github.com>
2024-10-05 10:43:16 -0300

    Add files via upload

---
+++ Definir Classes.js
@@ -0,0 +1,27 @@
+let heroi = {
+    nome: "Nyckollas",
+    idade: 15,
+    tipo: "ninja",
+}
+
+console.log(atacar());
+function atacar(){
+    let ataque;
+    switch(heroi.tipo){
+        case (heroi.tipo = "guerreiro"):
+        ataque = "espada";
+        break;
+        case (heroi.tipo = "mago"):
+        ataque = "magia";
+        break;
+        case (heroi.tipo = "monge"):
+            ataque = "artes marciais";
+            break;
+        case (heroi.tipo = "ninja"):
+            ataque = "shuriken";
+            break;
+        default:
+            console.log("Não existe esse tipo");
+    }
+        console.log(`o ${heroi.tipo} atacou usando ${ataque}`);
+}
